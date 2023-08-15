3d983e60c913bc9f999521a29787743f198a9b00
Wellington Della Mura <wellington@uenp.edu.br>
2023-08-15 17:09:19 -0300

    how to cube

--- 2d-images/projects/snake/app.js
+++ 2d-images/projects/snake/app.js
@@ -1,33 +1,59 @@
 const canvas = document.getElementById('myCanvas');
 const context = canvas.getContext('2d');
 const audioNotification = document.getElementById('audioNotification');
-
+const keys = {};
+const svgCache = {};
 
 //recebe o nome do arquivo svg, posição do canvas e o contexto 2d do canvas. Faz o fetch do arquivo e armazena em um cache para não precisar baixar novamente. Desenha a imagem do SVG no canvas a partir do contexto
 function drawSVG(svgName, x, y, context) {
-  if (!drawSVG.cache) {
-    drawSVG.cache = {};
-  }
-  if (!drawSVG.cache[svgName]) {
-    drawSVG.cache[svgName] = fetch(svgName)
+  if (!svgCache[svgName]) {
+    svgCache[svgName] = fetch(svgName)
       .then((response) => response.text())
       .then((svgText) => {
         const svg = new DOMParser().parseFromString(svgText, 'image/svg+xml');
         return svg.documentElement;
       });
   }
-  drawSVG.cache[svgName].then((svg) => {
+  svgCache[svgName].then((svg) => {
     context.drawImage(svg, x, y);
   });
 }
 
 //define os dados do círculo em forma de objeto
 const circle = {
-  circleSpeed: 10,
-  circleX: 50,
-  circleY: 50,
-  circleRadius: 50
+  speed: 10,
+  x: 50,
+  y: 50,
+  radius: 50,
+  draw : function (ctx) {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+    ctx.closePath();
+  },
+  //verifica se o círculo está dentro da área de colisão
+  testCollision: function (collisionArea) {
+    if (this.x + this.radius > collisionArea.x &&
+      this.x - this.radius < collisionArea.x + collisionArea.width &&
+      this.y + this.radius > collisionArea.y &&
+      this.y - this.radius < collisionArea.y + collisionArea.height) {
+      collisionArea.fill = 'red';
+      collisionArea.collision = true;
+      if (!collisionArea.soundPlayed) {
+        audioNotification.play();
+        collisionArea.soundPlayed = true;
+        score.points++;
+      }
+    } else {
+      collisionArea.fill = 'gray';
+      collisionArea.collision = false;
+      collisionArea.soundPlayed = false;
+    }
+  }  
 };
+
+
 
 //define os dados do score
 const score = {
@@ -35,7 +61,12 @@ const score = {
   x: 10,
   y: 30,
   font: '30px Arial',
-  fill: 'black'
+  fill: 'black',
+  draw: function (ctx) {
+    ctx.font = this.font;
+    ctx.fillStyle = this.fill;
+    ctx.fillText(`Pontos: ${this.points}`, this.x, this.y);
+  }  
 };
 
 //define uma area de colisão
@@ -46,65 +77,55 @@ const collisionArea = {
   height: 200,
   fill: 'gray',
   collision: false,
-  soundPlayed: false
+  soundPlayed: false,
+  draw: function (ctx) {
+    ctx.beginPath();
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = this.fill;
+    ctx.fill();
+    ctx.closePath();
+  }
 };
 
 // Função para desenhar a área de colisão
-function drawArea() {
-  context.beginPath();
-  context.rect(collisionArea.x, collisionArea.y, collisionArea.width, collisionArea.height);
-  context.fillStyle = collisionArea.fill;
-  context.fill();
-  context.closePath();
-}
 
 function adjustCanvasSize() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-
-  // Redesenhar o conteúdo do canvas após o ajuste
-  drawCircle();
 }
 
-// Função para desenhar o círculo
-function drawCircle() {
-  context.beginPath();
-  context.arc(circleX, circleY, circleRadius, 0, Math.PI * 2);
-  context.fillStyle = 'blue';
-  context.fill();
-  context.closePath();
-}
 
 // Função para atualizar a posição do círculo com base nas teclas pressionadas
-function updateCirclePosition() {
+function updateCirclePosition(circle, canvas, keys) {
   if (keys.ArrowUp) {
-    circleY -= circleSpeed;
+    circle.y -= circle.speed;
   }
   if (keys.ArrowDown) {
-    circleY += circleSpeed;
+    circle.y += circle.speed;
   }
   if (keys.ArrowLeft) {
-    circleX -= circleSpeed;
+    circle.x -= circle.speed;
   }
   if (keys.ArrowRight) {
-    circleX += circleSpeed;
+    circle.x += circle.speed;
   }
-  //se o círculo sair da tela, ele volta para o lado oposto
-  if (circleX > canvas.width) {
-    circleX = 0;
+
+  // Limitar a posição do círculo para não sair do canvas
+  if (circle.x - circle.radius < 0) {
+    circle.x = circle.radius;
   }
-  if (circleX < 0) {
-    circleX = canvas.width;
+  if (circle.x + circle.radius > canvas.width) {
+    circle.x = canvas.width - circle.radius;
   }
-  if (circleY > canvas.height) {
-    circleY = 0;
+  if (circle.y - circle.radius < 0) {
+    circle.y = circle.radius;
   }
-  if (circleY < 0) {
-    circleY = canvas.height;
-  }
+  if (circle.y + circle.radius > canvas.height) {
+    circle.y = canvas.height - circle.radius;
+  } 
 }
 
-const keys = {};
+
 
 // Capturar teclas pressionadas
 window.addEventListener('keydown', (event) => {
@@ -117,41 +138,18 @@ window.addEventListener('keyup', (event) => {
 });
 
 
-// Testa a colisão entre o circulo e a área de colisão
-function testCollision() {
-  if (circleX + circleRadius > collisionArea.x &&
-    circleX - circleRadius < collisionArea.x + collisionArea.width &&
-    circleY + circleRadius > collisionArea.y &&
-    circleY - circleRadius < collisionArea.y + collisionArea.height) {
-    collisionArea.fill = 'red';
-    collisionArea.collision = true;
-    if (!collisionArea.soundPlayed) {
-      audioNotification.play();
-      collisionArea.soundPlayed = true;
-      score.points++;
-    }
-  } else {
-    collisionArea.fill = 'gray';
-    collisionArea.collision = false;
-    collisionArea.soundPlayed = false;
-  }
-}
+//armazena todos os objetos a serem desenhados
+const drawables = [circle, score, collisionArea];
 
-// desenha o score
-function drawScore() {
-  context.font = score.font;
-  context.fillStyle = score.fill;
-  context.fillText('Score: ' + score.points, score.x, score.y);
-}
 
 // Função de animação usando requestAnimationFrame
 function animate() {
-  updateCirclePosition();
+  updateCirclePosition(circle, canvas, keys);
   testCollision();
   context.clearRect(0, 0, canvas.width, canvas.height);
-  drawScore();
-  drawArea();
-  drawCircle();
+  drawables.forEach((drawable) => {
+    drawable.draw(context);
+  });
   requestAnimationFrame(animate);
 }
 
---
+++ 3d-images/webgl/cube/how-to-cube/webgl-util.js
@@ -0,0 +1,72 @@
+//
+// Initialize a shader program, so WebGL knows how to draw our data
+//
+function initShaderProgram(gl, vsSource, fsSource) {
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+  // Create the shader program
+
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  // If creating the shader program failed, alert
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+    return null;
+  }
+
+  return shaderProgram;
+}
+
+//
+// creates a shader of the given type, uploads the source and
+// compiles it.
+//
+function loadShader(gl, type, source) {
+  const shader = gl.createShader(type);
+
+  // Send the source to the shader object
+
+  gl.shaderSource(shader, source);
+
+  // Compile the shader program
+
+  gl.compileShader(shader);
+
+  // See if it compiled successfully
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}
+
+
+
+//
+// WebGL Initialization
+//
+function getGLContext(elementId) {
+  const canvas = document.querySelector(elementId);
+
+  // Adjust Canvas to Available Screen Size
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  const gl = canvas.getContext('webgl');
+
+  // If we don't have a GL context, give up now
+
+  if (!gl) {
+    alert('Unable to initialize WebGL. Your browser or machine may not support it.');
+    return null;
+  }
+  return gl;
+}
