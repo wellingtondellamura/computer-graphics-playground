eb3ca4b610ad401e0eea4369db8b7977827bd7b0
Wellington Della Mura <della.mura@gmail.com>
2023-08-16 06:07:04 -0300

    refactor

--- 3d-images/webgl/cube/how-to-cube/cube.js
+++ 3d-images/webgl/cube/how-to-cube/cube.js
@@ -1,9 +1,64 @@
-let cubeRotation = 0.0;
+let cubeData = {
+  position: {
+    x: 0.0,
+    y: 0.0,
+    z: -10.0
+  },
+  speed: 0.0,
 
-let cubePosition = {
-  x: 0.0,
-  y: 0.0,
-  z: -10.0
+  faces : [
+    // Front face
+    -1.0, -1.0, 1.0,
+    1.0, -1.0, 1.0,
+    1.0, 1.0, 1.0,
+    -1.0, 1.0, 1.0,
+
+    // Back face
+    -1.0, -1.0, -1.0,
+    -1.0, 1.0, -1.0,
+    1.0, 1.0, -1.0,
+    1.0, -1.0, -1.0,
+
+    // Top face
+    -1.0, 1.0, -1.0,
+    -1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0,
+    1.0, 1.0, -1.0,
+
+    // Bottom face
+    -1.0, -1.0, -1.0,
+    1.0, -1.0, -1.0,
+    1.0, -1.0, 1.0,
+    -1.0, -1.0, 1.0,
+
+    // Right face
+    1.0, -1.0, -1.0,
+    1.0, 1.0, -1.0,
+    1.0, 1.0, 1.0,
+    1.0, -1.0, 1.0,
+
+    // Left face
+    -1.0, -1.0, -1.0,
+    -1.0, -1.0, 1.0,
+    -1.0, 1.0, 1.0,
+    -1.0, 1.0, -1.0,
+  ],
+  colors : [
+    [1.0, 0.0, 0.0, 1.0], //red
+    [0.0, 1.0, 0.0, 1.0], //green
+    [0.0, 0.0, 1.0, 1.0], //blue
+    [1.0, 1.0, 0.0, 1.0], //yellow
+    [1.0, 0.0, 1.0, 1.0], //purple
+    [0.0, 1.0, 1.0, 1.0], //cyan
+  ],
+  indices : [
+    0, 1, 2, 0, 2, 3,    // front
+    4, 5, 6, 4, 6, 7,    // back
+    8, 9, 10, 8, 10, 11,   // top
+    12, 13, 14, 12, 14, 15,   // bottom
+    16, 17, 18, 16, 18, 19,   // right
+    20, 21, 22, 20, 22, 23,   // left
+  ]
 };
 
 const vertexShaderSource = `
@@ -56,9 +111,9 @@ function main(elementId, vsSource, fsSource) {
 
   const buffers = initBuffers(gl);
   var then = 0;
-  // Draw the scene repeatedly
+
   function render(now) {
-    now *= 0.001;  // convert to seconds
+    now *= 0.001;
     const deltaTime = now - then;
     then = now;
     drawScene(gl, programInfo, buffers, deltaTime);
@@ -68,107 +123,18 @@ function main(elementId, vsSource, fsSource) {
 }
 
 function initBuffers(gl) {
-
-  // Create a buffer for the cube's vertex positions.
-
   const positionBuffer = gl.createBuffer();
-
-  // Select the positionBuffer as the one to apply buffer
-  // operations to from here out.
-
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cubeData.faces), gl.STATIC_DRAW);
 
-  // Now create an array of positions for the cube.
-
-  const positions = [
-    // Front face
-    -1.0, -1.0, 1.0,
-    1.0, -1.0, 1.0,
-    1.0, 1.0, 1.0,
-    -1.0, 1.0, 1.0,
-
-    // Back face
-    -1.0, -1.0, -1.0,
-    -1.0, 1.0, -1.0,
-    1.0, 1.0, -1.0,
-    1.0, -1.0, -1.0,
-
-    // Top face
-    -1.0, 1.0, -1.0,
-    -1.0, 1.0, 1.0,
-    1.0, 1.0, 1.0,
-    1.0, 1.0, -1.0,
-
-    // Bottom face
-    -1.0, -1.0, -1.0,
-    1.0, -1.0, -1.0,
-    1.0, -1.0, 1.0,
-    -1.0, -1.0, 1.0,
-
-    // Right face
-    1.0, -1.0, -1.0,
-    1.0, 1.0, -1.0,
-    1.0, 1.0, 1.0,
-    1.0, -1.0, 1.0,
-
-    // Left face
-    -1.0, -1.0, -1.0,
-    -1.0, -1.0, 1.0,
-    -1.0, 1.0, 1.0,
-    -1.0, 1.0, -1.0,
-  ];
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-  // Now set up the colors for the faces. We'll use solid colors
-  // for each face.
-
-  const faceColors = [
-    [1.0, 0.0, 0.0, 1.0], //red
-    [0.0, 1.0, 0.0, 1.0], //green
-    [0.0, 0.0, 1.0, 1.0], //blue
-    [1.0, 1.0, 0.0, 1.0], //yellow
-    [1.0, 0.0, 1.0, 1.0], //purple
-    [0.0, 1.0, 1.0, 1.0], //cyan
-  ];
-
-  // Convert the array of colors into a table for all the vertices.
-
-  var colors = [];
-
-  for (var j = 0; j < faceColors.length; ++j) {
-    const c = faceColors[j];
-
-    // Repeat each color four times for the four vertices of the face
-    colors = colors.concat(c, c, c, c);
-  }
 
   const colorBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-
-  // Build the element array buffer; this specifies the indices
-  // into the vertex arrays for each face's vertices.
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorsToTable(cubeData.colors)), gl.STATIC_DRAW);
 
   const indexBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-  // This array defines each face as two triangles, using the
-  // indices into the vertex array to specify each triangle's
-  // position.
-
-  const indices = [
-    0, 1, 2, 0, 2, 3,    // front
-    4, 5, 6, 4, 6, 7,    // back
-    8, 9, 10, 8, 10, 11,   // top
-    12, 13, 14, 12, 14, 15,   // bottom
-    16, 17, 18, 16, 18, 19,   // right
-    20, 21, 22, 20, 22, 23,   // left
-  ];
-
-  // Now send the element array to GL
-
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
-    new Uint16Array(indices), gl.STATIC_DRAW);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeData.indices), gl.STATIC_DRAW);
 
   return {
     position: positionBuffer,
@@ -177,17 +143,13 @@ function initBuffers(gl) {
   };
 }
 
-//
-// Draw the scene.
-//
 function drawScene(gl, programInfo, buffers, deltaTime) {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
   gl.clearDepth(1.0);                 // Clear everything
-  gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-  gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+  gl.enable(gl.DEPTH_TEST);           // The Depth Test is a per-sample processing operation performed after the Fragment Shader 
+  gl.depthFunc(gl.EQUAL);            // Near things obscure far things (https://pt.wikipedia.org/wiki/Z-buffer)
 
   // Clear the canvas before we start drawing on it.
-
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   // Create a perspective matrix, a special matrix that is
@@ -220,17 +182,18 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
 
   mat4.translate(modelViewMatrix,     // destination matrix
     modelViewMatrix,     // matrix to translate
-    [cubePosition.x, cubePosition.y, cubePosition.z]);  // amount to translate (x,y,z)
+    [cubeData.position.x, cubeData.position.y, cubeData.position.z]);  // amount to translate (x,y,z)
 
   mat4.rotate(modelViewMatrix,  // destination matrix
     modelViewMatrix,  // matrix to rotate
-    cubeRotation,     // amount to rotate in radians
+    cubeData.speed,     // amount to rotate in radians
     [1, 0, 1]);       // axis to rotate around (Z)
 
   mat4.rotate(modelViewMatrix,  // destination matrix
     modelViewMatrix,  // matrix to rotate
-    cubeRotation * .7,// amount to rotate in radians
+    cubeData.speed * .7,// amount to rotate in radians
     [0, 1, 0]);       // axis to rotate around (X)
+
 
   // Tell WebGL how to pull out the positions from the position
   // buffer into the vertexPosition attribute
@@ -291,13 +254,13 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
     false,
     modelViewMatrix);
 
-    const vertexCount = 36;
-    const type = gl.UNSIGNED_SHORT;
-    const offset = 0;
-    gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
-  
+  const vertexCount = 36;
+  const type = gl.UNSIGNED_SHORT;
+  const offset = 0;
+  gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+
   // Update the rotation for the next draw
 
-  cubeRotation += deltaTime;
+  cubeData.speed += deltaTime;
 }
 
--- 3d-images/webgl/cube/how-to-cube/webgl-util.js
+++ 3d-images/webgl/cube/how-to-cube/webgl-util.js
@@ -1,19 +1,10 @@
-//
-// Initialize a shader program, so WebGL knows how to draw our data
-//
 function initShaderProgram(gl, vsSource, fsSource) {
   const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
   const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
-
-  // Create the shader program
-
   const shaderProgram = gl.createProgram();
   gl.attachShader(shaderProgram, vertexShader);
   gl.attachShader(shaderProgram, fragmentShader);
   gl.linkProgram(shaderProgram);
-
-  // If creating the shader program failed, alert
-
   if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
     alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
     return null;
@@ -22,51 +13,37 @@ function initShaderProgram(gl, vsSource, fsSource) {
   return shaderProgram;
 }
 
-//
-// creates a shader of the given type, uploads the source and
-// compiles it.
-//
 function loadShader(gl, type, source) {
   const shader = gl.createShader(type);
-
-  // Send the source to the shader object
-
   gl.shaderSource(shader, source);
-
-  // Compile the shader program
-
   gl.compileShader(shader);
-
-  // See if it compiled successfully
-
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
     alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
     gl.deleteShader(shader);
     return null;
   }
-
   return shader;
 }
 
-
-
-//
-// WebGL Initialization
-//
 function getGLContext(elementId) {
   const canvas = document.querySelector(elementId);
-
-  // Adjust Canvas to Available Screen Size
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-
   const gl = canvas.getContext('webgl');
-
-  // If we don't have a GL context, give up now
-
   if (!gl) {
     alert('Unable to initialize WebGL. Your browser or machine may not support it.');
     return null;
   }
   return gl;
+}
+
+ // Convert the array of colors into a table for all the vertices.
+
+function colorsToTable(colors) {
+  var colorTable = [];
+  for (var j = 0; j < colors.length; j++) {
+    const c = colors[j];
+    colorTable = colorTable.concat(c, c, c, c);
+  }
+  return colorTable;
 }
