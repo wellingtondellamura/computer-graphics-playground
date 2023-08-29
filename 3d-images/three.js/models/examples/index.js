4dafd8b06b29918355adb97eb0c958dd505ecbbd
Wellington Della Mura <della.mura@gmail.com>
2023-08-29 07:01:13 -0300

    trex

--- 3d-images/three.js/models/examples/index.js
+++ 3d-images/three.js/models/examples/index.js
@@ -1,10 +1,15 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 // Configuração da cena
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -24,32 +29,58 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(0, 0, 2);
 scene.add(directionalLight);
 
-
-// carrega o modelo trex/scene.gltf, associa a um objeto chamado trex e adiciona no centro da cena 
+// carrega o modelo trex/scene.gltf, associa a um objeto chamado trex e adiciona no centro da cena
 const loader = new GLTFLoader();
-let trex;
-loader.load('trex/scene.gltf', function (gltf) {
-    trex = gltf.scene;
-    trex.position.set(0, 0, 0);
-    scene.add(trex);
-    //direciona a luz para o trex
-    directionalLight.target = trex;
-    console.log(trex);
-}, undefined, function (error) {
-    console.error(error);
-});
+let object;
+let mixer;
+loader.load(
+  "trex/scene.gltf",
+  function (gltf) {
+    object = gltf.scene;
+    scene.add(object);
+    directionalLight.target = object;
+    mixer = new THREE.AnimationMixer(object);
+    // Verifique se há animações no arquivo GLTF
+    if (gltf.animations && gltf.animations.length > 0) {
+      const animationButtonsDiv = document.getElementById("animationButtons");
 
+      gltf.animations.forEach((animation, index) => {
+        const button = document.createElement("button");
+        button.textContent = animation.name || `Animação ${index + 1}`;
+        button.addEventListener("click", () => {
+          // Iniciar a animação correspondente
+          console.log("Iniciando animação", animation.name);
+          console.log("mixer", mixer);
+          const clipAction = mixer.clipAction(animation);
+          console.log("clipAction", clipAction);
+          console.log("clipAction.play()", clipAction.play());
+        });
+        animationButtonsDiv.appendChild(button);
+      });
+    } else {
+      console.log("Nenhuma animação encontrada no arquivo GLTF.");
+    }
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
 
 // Função de animação
 const animate = () => {
-    requestAnimationFrame(animate);
-    //orbitcontrols
-    controls.update();
-    // Rotacionar o objeto trex
-     //trex.rotation.x += 0.01;
-     //trex.rotation.y += 0.01;
+  requestAnimationFrame(animate);
+  //orbitcontrols
+  controls.update();
+  // Atualize o mixer de animação a cada frame
+  if (mixer) {
+    mixer.update(0.01);
+  }
+  // Rotacionar o objeto trex
+  //trex.rotation.x += 0.01;
+  //trex.rotation.y += 0.01;
 
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 };
 
 animate();
